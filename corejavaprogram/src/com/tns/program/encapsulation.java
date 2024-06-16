b458152ae8b9123b92bc3fb51a2a16c4b3d1bf9d
kvvin <kvvin@vinny>
2024-06-16 17:31:16 +0530

    commit message

---
+++ corejavaprogram/src/com/tns/program/encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.program;
+
+public class encapsulation {
+
+	int serialnumber;   //properties or data members
+	String name;
+	int age;
+	
+	
+        public void show() {    //method or member function
+        	System.out.println("serialnumber"+serialnumber+"  "+"name  "+name+"  "+"age"+age);
+        }
+	
+}
