ccfcabb5526b35cdf12d94d3b516ba6fef484f2a
kvvin <kvvin@vinny>
2024-06-16 17:36:49 +0530

    commit message

---
+++ corejavaprogram/src/com/tns/program/exce.java
@@ -0,0 +1,14 @@
+package com.tns.program;
+
+public class exce {
+	
+	public static void main(String[]args){
+		encapsulation e=new encapsulation();
+		e.serialnumber=1;
+		e.name="vinuthna";
+		e.age=20;
+		
+		e.show(); //accessing the member function or method
+	}
+
+}
