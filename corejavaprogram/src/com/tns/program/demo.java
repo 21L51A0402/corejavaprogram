4f63c56d467e500a67e3c26a7a70d5eac1bcb4fb
kvvin <kvvin@vinny>
2024-06-16 16:48:36 +0530

    commit message

---
+++ corejavaprogram/src/com/tns/program/demo.java
@@ -0,0 +1,15 @@
+package com.tns.program;
+
+public class demo {
+	
+	  public static void main(String[]args) {
+		  publicexample u=new publicexample();
+		        
+		  System.out.println(u.a);
+		  System.out.println(u.f);
+		  
+		  
+		  
+	  }
+
+}
