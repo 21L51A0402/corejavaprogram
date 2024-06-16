0695258719c7cd16bfe1cbe8eb16077006c6321e
kvvin <kvvin@vinny>
2024-06-16 12:14:18 +0530

    commit message

---
+++ corejavaprogram/src/com/tns/program/publicexample.java
@@ -0,0 +1,18 @@
+package com.tns.program;
+
+public class publicexample {
+
+int a=9;
+
+        protected char f='y';     //data members or properties
+        
+        public static void main(String[]args) {
+        	publicexample u=new publicexample();     //object creation
+        	
+        	System.out.println(u.a);
+        	System.out.println(u.f);
+        	
+        }
+                
+
+}
