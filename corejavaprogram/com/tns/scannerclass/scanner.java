d8d1d6b5cf0dd5735ce645a3fa19c4e1ae0a7402
kvvin <kvvin@vinny>
2024-06-30 09:54:07 +0530

    commit message

---
+++ corejavaprogram/com/tns/scannerclass/scanner.java
@@ -0,0 +1,24 @@
+package com.tns.scannerclass;
+      
+import java.utli.scanner;
+       //demo of scanner class
+public class scannerexample{
+	    
+	   public static void main (String[] args){
+		     Scanner sc=new scanner(system.in);
+		     
+		     
+		  System.out.println("enter you are name");
+		      string name=sc.nextline();
+		  
+		      
+		      System.out.println("enter the student id");
+		      int sid=sc.nextlnt();
+		      
+		      
+		   System.out.println("enter percentage");
+		 float per=sc.nextFloat();
+	   }
+	
+
+}
--- corejavaprogram/src/com/tns/program/exce.java
+++ corejavaprogram/src/com/tns/program/exce.java
@@ -2,13 +2,11 @@ package com.tns.program;
 
 public class exce {
 	
-	public static void main(String[]args){
-		encapsulation e=new encapsulation();
-		e.serialnumber=1;
-		e.name="vinuthna";
-		e.age=20;
-		
-		e.show(); //accessing the member function or method
-	}
 
+public static void main (String[]args){
+	encapsulation e=new encapsulation();
+	e.serialnumber=1;
+	e.name="vinuthna";
+	e.age=20;
+}
 }
